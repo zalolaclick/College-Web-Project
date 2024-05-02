db7cf408b262162c44a4d20ea993996c6b3123b3
Zalola <haseebhassan814@gmail.com>
2024-05-02 13:40:30 +0000

    Chnages to button

--- Assets/Scripts/Theme.js
+++ Assets/Scripts/Theme.js
@@ -1,12 +1,18 @@
+var isDarkTheme = false;
+
 function toggleTheme() {
-    var lightTheme = document.getElementById('light-theme');
-    var darkTheme = document.getElementById('dark-theme');
-    
-    if (lightTheme.href.endsWith('light.css')) {
-      lightTheme.disabled = true;
-      darkTheme.disabled = false;
-    } else {
-      lightTheme.disabled = false;
-      darkTheme.disabled = true;
-    }
+  var lightTheme = document.getElementById('light-theme');
+  var darkTheme = document.getElementById('dark-theme');
+  
+  isDarkTheme = !isDarkTheme;
+  
+  if (isDarkTheme) {
+    lightTheme.disabled = true;
+    darkTheme.disabled = false;
+    themeButtonImage.src = '..\\..\\Assets\\Images\\Buttons\\Theme-Dark.png';
+  } else {
+    lightTheme.disabled = false;
+    darkTheme.disabled = true;
+    themeButtonImage.src = '..\\..\\Assets\\Images\\Buttons\\Theme-Light.png';
   }
+}
