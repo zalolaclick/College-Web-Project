e5345083618ff1c092fd29e0de597de73b96a9ed
Zalola <haseebhassan814@gmail.com>
2024-05-09 12:31:59 +0000

    Attempt at fixing theme.js

---
+++ docs/assets/Scripts/Theme-Home.js
@@ -0,0 +1,21 @@
+var isDarkTheme = false;
+
+function toggleTheme() {
+  var lightTheme = document.getElementById('light-theme');
+  var darkTheme = document.getElementById('dark-theme');
+  var themeButtonImage = document.getElementById("themeButtonImage");
+  
+  isDarkTheme = !isDarkTheme;
+  
+  if (isDarkTheme) {
+    lightTheme.disabled = true;
+    darkTheme.disabled = false;
+    themeButtonImage.src = "docs/assets/Images/Buttons/Theme-Light.png";
+    localStorage.setItem("theme", "dark");
+  } else {
+    lightTheme.disabled = false;
+    darkTheme.disabled = true;
+    themeButtonImage.src = "docs/assets/Images/Buttons/Theme-Dark.png";
+    localStorage.setItem("theme", "light");
+  };
+};
