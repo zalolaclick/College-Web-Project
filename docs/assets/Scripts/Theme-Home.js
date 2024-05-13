00d24940761a4e42f8b5fd2d3241be58d9e341ac
Zalola <haseebhassan814@gmail.com>
2024-05-13 14:46:02 +0000

    Fixes Themes and Improves Home Title

--- docs/assets/Scripts/Theme-Home.js
+++ docs/assets/Scripts/Theme-Home.js
@@ -4,6 +4,7 @@ function toggleTheme() {
   var lightTheme = document.getElementById('light-theme');
   var darkTheme = document.getElementById('dark-theme');
   var themeButtonImage = document.getElementById("themeButtonImage");
+  var homeTitle = document.getElementById("homeTitle");
   
   isDarkTheme = !isDarkTheme;
   
@@ -11,11 +12,13 @@ function toggleTheme() {
     lightTheme.disabled = true;
     darkTheme.disabled = false;
     themeButtonImage.src = "docs/assets/Images/Buttons/Theme-Light.png";
+    homeTitle.src = "docs/assets/Images/Home-Title/Home-Title-New-Dark.png.png";
     localStorage.setItem("theme", "dark");
   } else {
     lightTheme.disabled = false;
     darkTheme.disabled = true;
     themeButtonImage.src = "docs/assets/Images/Buttons/Theme-Dark.png";
+    homeTitle.src = "docs/assets/Images/Home-Title/Home-Title-New-Light.png.png";
     localStorage.setItem("theme", "light");
   };
 };
