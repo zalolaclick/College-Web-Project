d5485c959657855fde28144355c272fac1dc8001
Zalola <haseebhassan814@gmail.com>
2024-05-15 10:06:57 +0000

    Adds phone to contact, Breaks NavBar.

--- docs/assets/Scripts/Theme.js
+++ docs/assets/Scripts/Theme.js
@@ -4,6 +4,14 @@ function toggleTheme() {
   var lightTheme = document.getElementById('light-theme');
   var darkTheme = document.getElementById('dark-theme');
   var themeButtonImage = document.getElementById("themeButtonImage");
+  var htmlthemeid = document.getElementsByTagName("HTML")
+  if (htmlthemeid.id == "about") {
+    var placeholder1 = 0;
+  } else if (htmlthemeid.id == "job") {
+    var placeholder1 = 0;
+  } else {
+    phonetheme = document.getElementById("phone")
+  };
   
   isDarkTheme = !isDarkTheme;
   
@@ -12,10 +20,24 @@ function toggleTheme() {
     darkTheme.disabled = false;
     themeButtonImage.src = "../docs/assets/Images/Buttons/Theme-Light.png";
     localStorage.setItem("theme", "dark");
+    if (htmlthemeid.id == "about") {
+      var placeholder1 = 0;
+    } else if (htmlthemeid.id == "job") {
+      var placeholder1 = 0;
+    } else {
+      phonetheme.src = "../docs/assets/Images/Contact/Phone-Dark.png";
+    };
   } else {
     lightTheme.disabled = false;
     darkTheme.disabled = true;
     themeButtonImage.src = "../docs/assets/Images/Buttons/Theme-Dark.png";
     localStorage.setItem("theme", "light");
+    if (htmlthemeid.id == "about") {
+      var placeholder1 = 0;
+    } else if (htmlthemeid.id == "job") {
+      var placeholder1 = 0;
+    } else {
+      phonetheme.src = "../docs/assets/Images/Contact/Phone-Light.png";
+    };
   };
 };
