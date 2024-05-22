ca17564c10243f63e31964d4b94faa5e37905faa
Zalola <haseebhassan814@gmail.com>
2024-05-22 10:44:10 +0000

    Adds Portfolio

--- docs/assets/Scripts/Theme.js
+++ docs/assets/Scripts/Theme.js
@@ -6,9 +6,9 @@
 var IsDarkTheme = false;
 console.log("Variable IsDarkTheme Loaded");
 var TitleImage; // Unset because this is not universal to all pages.
-console.log("Variable TitleImage Loaded");
+console.log("Variable TitleImage Ready");
 var PhoneTheme; // Unset because this is not universal to all pages.
-console.log("Variable PhoneTheme Loaded");
+console.log("Variable PhoneTheme Ready");
 
 // To change the theme of the website.
 function toggleTheme() {
@@ -20,7 +20,6 @@ function toggleTheme() {
   console.log("Theme Changer Button Loaded");
   var PageTheme = document.getElementsByTagName("HTML")[0].id;
   console.log("PageTheme: ", PageTheme);
-  console.log("Determining Page:");
   // Sets variables that a specific to certain pages.
   if (PageTheme == "home") {
     TitleImage = document.getElementById("TitleImage");
@@ -28,7 +27,10 @@ function toggleTheme() {
   } else if (PageTheme == "about") {
     placeholder1 = 0;
   } else if (PageTheme == "portfolio") {
-    placeholder1 = 0;
+    var Employee1 = document.getElementById("Employee1");
+    var Employee2 = document.getElementById("Employee2");
+    var Employee3 = document.getElementById("Employee3");
+    var Employee4 = document.getElementById("Employee4");
   } else if (PageTheme == "job") {
     placeholder1 = 0;
   } else if (PageTheme == "apply") {
@@ -40,7 +42,7 @@ function toggleTheme() {
   IsDarkTheme = !IsDarkTheme;
 
   // For pages: About, Job, and Contact // Changes to dark theme.
-  if (IsDarkTheme && PageTheme != ("home" || "portfolio" || "apply")) {
+  if (IsDarkTheme && (PageTheme == "about" || PageTheme == "job" || PageTheme == "contact")) {
     // Elements that appear on all pages.
     lightTheme.disabled = true;
     darkTheme.disabled = false;
@@ -54,13 +56,13 @@ function toggleTheme() {
     } else if (PageTheme == "job") {
       // Elements specific to: Job.
       var placeholder1; // !Unfinished Page!
-    } else {
+    } else if (PageTheme == "contact") {
       // Elements specific to: Contact.
       PhoneTheme.src = "../docs/assets/Images/Contact/Phone-Dark.png";
       console.log("Phone changed to: " + PhoneTheme.src);
     }
     // For pages: About, Contact and Job // Changes to light theme.
-  } else if (PageTheme != ("home" || "portfolio" || "apply")) {
+  } else if (PageTheme == "about" || PageTheme == "job" || PageTheme == "contact") {
     // Elements that appear on all pages.
     lightTheme.disabled = false;
     darkTheme.disabled = true;
@@ -85,10 +87,16 @@ function toggleTheme() {
     lightTheme.disabled = true;
     darkTheme.disabled = false;
     console.log("Theme changed to dark.");
-    themeButtonImage.src = "../docs/assets/Images/Buttons/Theme-Light.png";
+    themeButtonImage.src = "../../docs/assets/Images/Buttons/Theme-Light.png";
     console.log("Theme Changing Button Image is: " + themeButtonImage.src);
     localStorage.setItem("theme", "dark");
-    // !Both pages unfinished!
+    if (PageTheme == "portfolio") {
+      // Elements specific to: Portfolio.
+      Employee1.src = "../../docs/assets/Images/Portfolio/Employee1.png";
+      Employee2.src = "../../docs/assets/Images/Portfolio/Employee2.png";
+      Employee3.src = "../../docs/assets/Images/Portfolio/Employee3.png";
+      Employee4.src = "../../docs/assets/Images/Portfolio/Employee4.png";
+    }
     // For pages: Portfolio and Apply // Changes to light theme.
   } else if (PageTheme != "home") {
     // Elements that appear on all pages.
@@ -98,9 +106,15 @@ function toggleTheme() {
     themeButtonImage.src = "../../docs/assets/Images/Buttons/Theme-Dark.png";
     console.log("Theme Changing Button Image is: " + themeButtonImage.src);
     localStorage.setItem("theme", "light");
-    // !Both pages unfinished!
+    if (PageTheme == "portfolio") {
+      // Elements specific to: Portfolio.
+      Employee1.src = "../../docs/assets/Images/Portfolio/Employee1-Dark.png";
+      Employee2.src = "../../docs/assets/Images/Portfolio/Employee2-Dark.png";
+      Employee3.src = "../../docs/assets/Images/Portfolio/Employee3-Dark.png";
+      Employee4.src = "../../docs/assets/Images/Portfolio/Employee4-Dark.png";
+    }
     // For pages: Home // Changes to dark theme.
-  } else if (IsDarkTheme) {
+  } else if (IsDarkTheme && PageTheme == "home") {
     // Elements that appear on all pages.
     lightTheme.disabled = true;
     darkTheme.disabled = false;
@@ -112,7 +126,7 @@ function toggleTheme() {
     TitleImage.src = "docs/assets/Images/Home/Home-Title-New-Dark.png";
     console.log("Home's Title changed to: " + TitleImage.src);
     // For pages: Home // Changes to light theme.
-  } else {
+  } else if (PageTheme == "home") {
     // Elements that appear on all pages.
     lightTheme.disabled = false;
     darkTheme.disabled = true;
